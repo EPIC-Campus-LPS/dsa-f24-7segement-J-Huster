cae2f36f09fa5adc00835257f1a51946bf0ff2d2
J-Huster <139395235+J-Huster@users.noreply.github.com>
2024-10-15 10:59:47 -0600

    Update cleanup.py
    Get rid of the while loop that turns on GPIO pins before turning them off

--- cleanup.py
+++ cleanup.py
@@ -1,21 +1,13 @@
 import RPi.GPIO as GPIO
-import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-x = 1
 y = 1
 
-while x < 40:
-	GPIO.setup(x,GPIO.OUT)
-	GPIO.output(x,GPIO.HIGH)
-	print("Working, LED on")
-	x += 1
-
-time.sleep(5)
-
 while y < 40:
+	GPIO.setup(y, GPIO.OUT)
 	GPIO.output(y,GPIO.LOW)
 	print("Cleanup, LED off")
 	y += 1
+
 print("Done")
