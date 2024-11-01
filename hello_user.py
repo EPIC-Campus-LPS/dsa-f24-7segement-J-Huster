483636eab7b9950aa09d8f2db3716f0a6eb2346d
J-Huster <139395235+J-Huster@users.noreply.github.com>
2024-11-01 09:37:30 -0600

    Update hello_user.py with comments

--- hello_user.py
+++ hello_user.py
@@ -13,9 +13,15 @@ clear = [0, 0, 0, 0] #Defining values used to clear
 name = input("Enter name: ")
 
 #Displaying "Hello Name" in display
+#Clear the display
 tm.write(clear)
+#Pause the program for one second
 time.sleep(1)
+#Assign the word Hello to the variable s
 s = 'Hello '
+#Scroll the word Hello across the display
 tm.scroll(s, delay=250)
+#Scroll the user input name across the display
 tm.scroll(name, delay=250)
+#Pause the program for two seconds
 time.sleep(2)
